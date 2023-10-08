447762b6cadf47d33de01d1c5889c7a6b170b780
karikumutimahonore <karikumutimahonore778@gmail.com>
2023-10-08 08:05:14 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry function
+ *
+ * Description: This program prints a message to the standard error stream.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	write(
+		2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19") - 1
+	);
+	return (1);
+}
+
