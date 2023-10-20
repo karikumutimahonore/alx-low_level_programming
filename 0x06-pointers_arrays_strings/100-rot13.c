c31a63da8d18ad877b921b3452d29304a48f18b7
karikumutimahonore <karikumutimahonore778@gmail.com>
2023-10-20 08:44:19 -0700

    function that encodes a string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @str: encoded string.
+ *
+ * Return: encoded string.
+ */
+char *rot13(char *str)
+{
+	int index_1 = 0, index_2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[index_1])
+	{
+		for (index_2 = 0; index_2 < 52; index_2++)
+		{
+			if (str[index_1] == alphabet[index_2])
+			{
+				str[index_1] = rot13key[index_2];
+				break;
+			}
+		}
+
+		index_1++;
+
+	}
+	return (str);
+}
