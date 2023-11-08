740493bfba164e4c6d1430da7f22aeaec38316d4
karikumutimahonore <karikumutimahonore778@gmail.com>
2023-11-08 08:51:54 -0800

    print my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 
+
 /**
  * print_name - function that prints a name
  * @name: name to be declared
@@ -11,3 +12,4 @@ void print_name(char *name, void (*f)(char *))
 		return;
 	f(name);
 }
+
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that Prints simple operations.
+ * @argc: arguments supplied to the program.
+ * @argv: arguments vector.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *ops;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	ops = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_ops_func(ops) == NULL || ops[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*ops == '/' && num2 == 0) ||
+	    (*ops == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_ops_func(ops)(num1, num2));
+
+	return (0);
+}
