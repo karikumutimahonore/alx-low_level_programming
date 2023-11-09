ba7445c1483594b799a8c9b757bce42404b08467
karikumutimahonore <karikumutimahonore778@gmail.com>
2023-11-09 04:34:25 -0800

    prototype functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/*
+ * Desc: Header file that containing all prototypes 
+ * used in this variadic functions project.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
